7af0c47134e015a9c9b09fac28ca32eaec253d8b
abdullahwaqas <abdullahwaqas1992@gmail.com>
2017-02-12 01:02:08 +0500

    login

---
+++ eCom/customUIImage.swift
@@ -0,0 +1,30 @@
+//
+//  customUIImage.swift
+//  eCom
+//
+//  Created by Abdullah on 11/02/2017.
+//  Copyright © 2017 Abdullah. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class customUIImage: UIImageView {
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+}
+
